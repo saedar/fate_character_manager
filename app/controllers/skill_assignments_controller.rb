c17b6face91645751c8cab23f7e56c716560dafe
RP029213 <ryan.perrin@cerner.com>
2014-05-15 16:03:50 -0500

    Updated for remote server. Updated skill assignment editing.

--- app/controllers/skill_assignments_controller.rb
+++ app/controllers/skill_assignments_controller.rb
@@ -57,7 +57,8 @@ class SkillAssignmentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_skill_assignment
-      @skill_assignment = SkillAssignment.find(params[:id])
+      @character = Character.find(params[:character_id])
+      @skill_assignment = @character.skill_assignments.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
